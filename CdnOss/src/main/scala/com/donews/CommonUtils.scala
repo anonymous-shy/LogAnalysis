1f5df9b460240ea520bf3d71a1fa8818f51d275f
anonymous-shy <shenhuayu0417@gmail.com>
2018-08-30 15:17:32 +0800

    init

---
+++ CdnOss/src/main/scala/com/donews/CommonUtils.scala
@@ -0,0 +1,22 @@
+package com.donews
+
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+/**
+  * Created by Shy on 2018/8/24
+  */
+
+object CommonUtils {
+
+  def parseLocalDate(dateString: String): LocalDate = LocalDate.parse(dateString, DateTimeFormatter.ISO_LOCAL_DATE)
+
+  def plusDays(date: String, days: Int): String = {
+    val localDate = parseLocalDate(date)
+    localDate.plusDays(days).format(DateTimeFormatter.ISO_LOCAL_DATE)
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(plusDays("2018-08-25", -3))
+  }
+}
