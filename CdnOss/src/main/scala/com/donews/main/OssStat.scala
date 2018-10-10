06c33862c9c2ae4eb97af3e82045cf3bf6748ce3
anonymous-shy <shenhuayu0417@gmail.com>
2018-10-10 14:09:38 +0800

    revert type

--- CdnOss/src/main/scala/com/donews/main/OssStat.scala
+++ CdnOss/src/main/scala/com/donews/main/OssStat.scala
@@ -1,5 +1,7 @@
 package com.donews.main
 
+import java.time.LocalDate
+
 import org.apache.spark.sql.SparkSession
 import org.slf4j.{Logger, LoggerFactory}
 
@@ -18,11 +20,51 @@ object OssStat {
       .appName(getClass.getSimpleName)
       .config("spark.sql.warehouse.dir", warehouseLocation)
       .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
-      .config("spark.sql.shuffle.partitions", "240")
+      .config("spark.sql.shuffle.partitions", "2000")
       .enableHiveSupport()
       .getOrCreate()
 
-    val ossData = spark.read.textFile("/data/cdn_oss/oss/oss_file/OssData-data")
+    //    val ossAll = spark.read.textFile("/data/cdn_oss/oss/oss_file/OssData-data").selectExpr("lower(value) as oss_uriA")
+    //    val ossOnline = spark.sql("select file_uri as oss_uriO from logs.online_og")
+    //
+    //    ossAll.join(ossOnline, ossAll("oss_uriA") === ossOnline("oss_uriO"), "left")
+    //      .write.parquet("/data/cdn_oss/output/OssData.parquet")
+    val currDate = LocalDate.now.toString
 
+    spark.udf.register("gen_url", (url: String) => {
+      var tmpUrl = url
+      if (url != null && url.startsWith("."))
+        tmpUrl = url.substring(1)
+      tmpUrl
+    })
+    val ossA = spark.read.parquet("/data/cdn_oss/output/OssData.parquet")
+      .where("oss_uriO is not null")
+      .selectExpr("concat('/',oss_uriO) as oss_uriO")
+
+    val img = spark.read.csv("/data/cdn_oss/mongo/article_photo.csv")
+      .selectExpr("_c0 as img_url", "_c1 as crawl_date", "gen_url(lower(_c2)) as saved_img_url")
+    val video = spark.read.csv("/data/cdn_oss/mongo/article_video.csv")
+      .selectExpr("_c0 as crawl_date", "gen_url(lower(_c1)) as saved_img_url", "_c2 as img_url")
+
+    val imgJoin = img.join(ossA, img("saved_img_url") === ossA("oss_uriO"), "left_outer")
+    imgJoin.createOrReplaceTempView("img")
+    spark.sql(
+      s"""
+         | SELECT img_url,crawl_date,saved_img_url,'$currDate' as process_date,
+         | case when oss_uriO IS NOT NULL then '1'
+         |      when oss_uriO IS NULL then '0'
+         | end onlineSign
+         | FROM img
+      """.stripMargin).write.json("/data/cdn_oss/output/oss_online/oss_online_img")
+
+    video.join(ossA, video("saved_img_url") === ossA("oss_uriO"), "left_outer").createOrReplaceTempView("video")
+    spark.sql(
+      s"""
+         | SELECT img_url,crawl_date,saved_img_url,'$currDate' as process_date,
+         | case when oss_uriO IS NOT NULL then '1'
+         |      when oss_uriO IS NULL then '0'
+         | end onlineSign
+         | FROM video
+      """.stripMargin).write.json("/data/cdn_oss/output/oss_online/oss_online_video")
   }
 }
--- CdnOss/src/test/scala/TestReplace.scala
+++ CdnOss/src/test/scala/TestReplace.scala
@@ -5,8 +5,8 @@
 
 
 object TestReplace extends App {
-  println(getUrl("[\"data/shareimg_oss/new_thumb/ylzx-fh-2/thumb_84837a5e0298bf30cdcf5f8c9e07fe0f.png\""))
-
+  //println(getUrl("[\"data/shareimg_oss/new_thumb/ylzx-fh-2/thumb_84837a5e0298bf30cdcf5f8c9e07fe0f.png\""))
+  println(genUrl2("./big_media_img/2017/03/23/4f04088f-2574-11e7-8e6e-f45c89baa8c7.jpeg"))
 
   def getUrl(url: String): String = {
     //    var tmpUrl = url
@@ -24,9 +24,16 @@ object TestReplace extends App {
     //      println("]:    " + tmpUrl)
     //    }
     val tmpUrl = url match {
-      case url if (url.contains("[\"") && url.contains("\"]")) => url.replace("[\"","").replace("\"]","")
+      case url if (url.contains("[\"") && url.contains("\"]")) => url.replace("[\"", "").replace("\"]", "")
     }
 
+    tmpUrl
+  }
+
+  def genUrl2(url: String): String = {
+    var tmpUrl = url
+    if (url.startsWith("."))
+      tmpUrl = url.substring(1)
     tmpUrl
   }
 }
